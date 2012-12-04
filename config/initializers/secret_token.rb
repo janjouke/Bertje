ee0b77c3d4388f45663ba81f6e64d1008820059c
Jan Jouke Harms <janjouke@gmail.com>
2012-12-04 19:01:26 +0100

    Add files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bertje::Application.config.secret_token = '2e504d0d6f73f7674bf648272b6f0e8cb08b7fb04efbbf636311330985f2a8f99ca13a3553aca29af475948ec2ac416743ba4463dffd43237a5dc9f5d3fa89cf'
